0a1d84d6ef73f9d207cb4a2b8faf1f27d8bec209
ThisIsMyWay <skaczacymuchomor@gmail.com>
2017-06-09 18:59:48 +0200

    initial list

---
+++ Hackaton_app/Hackaton_app/Controllers/HotelListController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackaton_app.Controllers
+{
+    public class HotelListController : Controller
+    {
+        // GET: HotelList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HotelList()
+        {
+
+            Models.HotelListModel hotelListData = new Models.HotelListModel()
+            {
+                Hotels = new List<Models.HotelItem>
+            {
+                new Models.HotelItem() { Name = "Gotyk House", Address = "Barowa 4" },
+                new Models.HotelItem() { Name = "Some House", Address = "Piwna 4", }
+
+            }
+            };             
+            return View("HotelListView", hotelListData);
+
+        }
+    }
+}
---
+++ Hackaton_app/Hackaton_app/Models/HotelListModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackaton_app.Models
+{
+    public enum DisabilityType
+    {
+
+    }
+
+    public enum Rate
+    {
+        Poor,
+        Fair,
+        Good,
+        Very_Good,
+        Excellent
+    }
+
+    public enum FacilitiesType
+    {
+    }
+
+    public class HotelListModel {
+       public   List<HotelItem> Hotels { get; set; }
+    }
+
+    public class HotelItem
+    {
+        public Dictionary<DisabilityType, int> DisabilityAndRateMap { get; set; }
+        public String Name { get; set; }
+        public String UrlToPicture { get; set; }
+        public Rate HotelRate { get; set; }
+        public Double Price { get; set; }
+        public String Address { get; set; }
+        public Dictionary<FacilitiesType, String> facilitiesAndIcon { get; set; }
+
+    }
+}
