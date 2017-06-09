d5f0bfb2493fb5b01e38e72909f266cc0fab2645
Daniel Rudziński <Dano19@o2.pl>
2017-06-09 15:11:33 +0200

    added text2speech to layout. Requires t2s class on div to say content.

--- Hackaton_app/Hackaton_app/App_Start/BundleConfig.cs
+++ Hackaton_app/Hackaton_app/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Hackaton_app
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
--- Hackaton_app/Hackaton_app/Controllers/HomeController.cs
+++ Hackaton_app/Hackaton_app/Controllers/HomeController.cs
@@ -27,34 +27,39 @@ namespace Hackaton_app.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            using (var synth = new SpeechSynthesizer())
-            {
-                foreach (var voice in synth.GetInstalledVoices())
-                {
-                    VoiceInfo info = voice.VoiceInfo;
-                    Debug.WriteLine(" Name:          " + info.Name);
-                    Debug.WriteLine(" Culture:       " + info.Culture);
-                    Debug.WriteLine(" Age:           " + info.Age);
-                    Debug.WriteLine(" Gender:        " + info.Gender);
-                    Debug.WriteLine(" Description:   " + info.Description);
-                    Debug.WriteLine(" ID:            " + info.Id);
-                    Debug.WriteLine(" Enabled:       " + voice.Enabled);
-                }
-            }
                 return View();
         }
 
-        public async Task<ActionResult> Text2Speech(string text)
+        //public async Task<ActionResult> T2STest()
+        //{
+        //    using (var synth = new SpeechSynthesizer())
+        //    {
+        //        foreach (var voice in synth.GetInstalledVoices())
+        //        {
+        //            VoiceInfo info = voice.VoiceInfo;
+        //            Debug.WriteLine(" Name:          " + info.Name);
+        //            Debug.WriteLine(" Culture:       " + info.Culture);
+        //            Debug.WriteLine(" Age:           " + info.Age);
+        //            Debug.WriteLine(" Gender:        " + info.Gender);
+        //            Debug.WriteLine(" Description:   " + info.Description);
+        //            Debug.WriteLine(" ID:            " + info.Id);
+        //            Debug.WriteLine(" Enabled:       " + voice.Enabled);
+        //        }
+        //    }
+        //    return View();
+        //}
+        
+        public async Task<ActionResult> Text2Speech(string text = "")
         {
             Task<FileContentResult> task = Task.Run(() =>
             {
                 using (var synth = new SpeechSynthesizer())
                 {
-                    synth.SelectVoice("Microsoft Sabina Desktop");
+                    synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
                     using (var stream = new MemoryStream())
                     {
                         synth.SetOutputToWaveStream(stream);
-                        synth.Speak("Traveling to: Gdansk");
+                        synth.Speak(text);
                         byte[] bytes = stream.GetBuffer();
                         return File(bytes, "audio/mpeg", "text.mp3");
                     }
