3951cbe5f948f5a058178a83984e1f323c9a1780
Daniel Rudziński <Dano19@o2.pl>
2017-06-10 03:13:47 +0200

    Adding new venue finished. Added uploads of images.

--- Hackaton_app/CommonLibrary/Logic/Venue.cs
+++ Hackaton_app/CommonLibrary/Logic/Venue.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonLibrary.Utility;
 
 namespace CommonLibrary.Logic
 {
@@ -18,6 +19,31 @@ namespace CommonLibrary.Logic
         {
             using (var db = new DatabaseContent())
                 return db.Venues.ToList();
-        }    
+        }
+
+        public static StatusResult Create(string name, int type, string description, string country, string city, string street, string phone, string email, string facebook, string website, string mapsPlaceId)
+        {
+            using (var db = new DatabaseContent())
+            {
+                var venue = new Database.Venue()
+                {
+                    Name = name,
+                    Type = type,
+                    Description = description,
+                    AddressCountry = country,
+                    AddressCity = city,
+                    AddressStreet = street,
+                    Website = website,
+                    PhoneNumber = phone,
+                    Email = email,
+                    Facebook = facebook,
+                    PlaceId = mapsPlaceId, 
+                    RevisionDate = DateTime.Now
+                };
+                db.Venues.Add(venue);
+                db.SaveChanges();
+                return new StatusResult() { Message = "Venue has been added!", Success = true, ReturnId = venue.Id };
+            }
+        }
     }
 }
--- Hackaton_app/Hackaton_app/Controllers/VenuesController.cs
+++ Hackaton_app/Hackaton_app/Controllers/VenuesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,15 +30,28 @@ namespace Hackaton_app.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            return View(CommonLibrary.Logic.Type.GetList());
         }
         [HttpPost]
-        public ActionResult Create(string name, int type, string description, string country, string city, string street, string phone, string email, string facebook, string mapsPlaceId)
+        public ActionResult Create(string name, int type, string description, string country, string city, string street, string phone, string email, string website, string facebook, string mapsPlaceId, HttpPostedFileBase[] files)
         {
-            return View();
+            var result = Venue.Create(name, type, description, country, city, street, phone, email, facebook, website, mapsPlaceId);
+            TempData["message"] = result;
+            if (result.Success)
+            {
+                foreach (var item in files)
+                {
+                    var filename = CommonLibrary.Utility.Security.RandomKey(item.FileName) + $"{Path.GetExtension(item.FileName)}";
+                    item.SaveAs($"{Server.MapPath("~/Content/hotels/")}{filename}");
+                    Media.SaveToDatabase(result.ReturnId, filename);
+                    // TODO: Improve this into drag and drop / Saving the file on server in temp and removing of adding failed / Save in batches
+                }
+                return RedirectToAction("Details", "Venues", new { id = result.ReturnId });
+            }
+            return RedirectToAction("Create", "Venues");
         }
 #endregion
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
             return View("Details");
         }
